aa849c17c0e8b1df3c22f87464c355729ff46bbf
Tony Vu <vuductrung2003@gmail.com>
2014-08-02 18:37:35 -0400

    sue site

--- call-bootstrap.php
+++ call-bootstrap.php
@@ -1,3 +1,6 @@
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <!-- Bootstrap core CSS -->
 <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 <link href="style.css" rel="stylesheet">
