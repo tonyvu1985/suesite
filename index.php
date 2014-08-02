bfa5eeeeef97c6f17d3420602976cda47f0d37f0
Tony Vu <vuductrung2003@gmail.com>
2014-08-02 02:34:17 -0400

    sue site

---
+++ index.php
@@ -0,0 +1,67 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="from dada to gaga">
+    <meta name="tony vu" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>From Dada To Gaga</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Google Font --!>
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Teko:500,600' rel='stylesheet' type='text/css'>
+  </head>
+<body>
+<div class="container">
+<!-- Call Narbar -->
+<?php include_once('narbar.php'); ?>
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="row equal">
+		<div class="col-md-6 col-xs-12 homepage-left">
+			<img class="img-responsive" src="images/home1.jpg" alt="" />
+		</div>
+		<div class="col-md-6 col-xs-12 homepage-right">
+			<p class="text-center"><img src="images/exhibition.jpg" alt="" /></p>
+			<div class="text-right exhibitiondetail col-xs-8 ">
+				<h1>Exhibition</h1>
+				<p>Chrissie Cotter Gallery<br/>
+				Pidcock Street, Camperdown<br/>
+				Opening night 25 September, 6pm<br/>
+				Thurs-Sun 11am-5pm until 05 October 2014</p>
+		</div>
+      </div><!-- End of Main Content -->
+    
+      <div class="col-xs-12 footer text-center">
+	<p><span class="name">Madam L'Ashby DADAist & Glomesher Extraordinaire</span><br/> 
+	<a class="email" href="mailto:madam@madamlashby.com">madam@madamlashby.com</a></p>
+      </div>
+</div> <!-- /container -->
+
+
+  </body>
+</html>
+
+</body>
+</html>
---
+++ narbar.php
@@ -0,0 +1,34 @@
+<?php 
+
+// Narbar 
+
+?>
+<!-- Static navbar -->
+<div class="navbar navbar-default" role="navigation">
+<div class="container-fluid">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Home</a>
+  </div>
+  <div class="navbar-collapse collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Exhibition</a></li>
+      <li><a href="#">Gallery</a></li>
+     <li><a href="glomesh.html">Glomesh</a></li>
+      <li><a href="dada.html">DADA</a></li>
+      <li><a href="about.html">About Madam L'Ashby</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="pt"><a class="pintrest" href="http://www.pinterest.com/madamlashby/" target="_blank"></a></li>
+      <li class="fb"><a class="facebook" href="https://www.facebook.com/MadamLashby" target="_blank"></a></li>
+      <li class="gp"><a class="googleplus" href="https://plus.google.com/u/0/101915113030661771747/posts" target="_blank"></a></li>
+      <li class="ig"><a class="instagram" href="http://instagram.com/madamlashby" target="_blank"></a></li>
+    </ul>
+  </div><!--/.nav-collapse -->
+</div><!--/.container-fluid -->
+
