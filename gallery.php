2aaf5fd3f62f3be9fc8afaecef43c50f53848965
Tony Vu <vuductrung2003@gmail.com>
2014-08-02 07:24:53 -0400

    sue site

--- gallery.php
+++ gallery.php
@@ -3,56 +3,90 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Gallery</title>
+<?php include_once('call-bootstrap.php'); ?>
 </head>
 
 <body>
+<style>
+.nav .gallery{background: #cb3338 !important; color:#000 !important}
+</style>
 
-
-<section>
-
-	<figure> <img src="images/gallery1" alt="Adoption 1"></figure>
-	<figure> <img src="images/"></figure>
-
-
-	<aside>
-		<h1>The L’Ash Gallery</h1>
-		<p> A retrospective and installation featuring photomontage artworks and Glomesh articles.  </p>
+<div class="container">
+<!-- Call Narbar -->
+<?php include_once('narbar.php'); ?>
+<div class="row equal">
+	<div class="col-md-8 col-xs-12 gallery-left">
+		<img class="img-responsive" src="images/gallery1" alt="Adoption 1">
+		<img src="images/">
+	</div>
+	<div class="col-md-4 col-xs-12 gallery-right">
+		<h1 class="title" style="color:#daa521">The L’Ash Gallery</h1>
+		<p>A retrospective and installation featuring photomontage artworks and Glomesh articles.  </p>
 		
-		<h2>List of artworks</h2>
+		<h1 class="title" style="color:#daa521">List of artworks</h2>
+		<table class="table table-no-border">
+			<tr>
+				<td>Bonkers in Honkers</td>
+				<td>1995</td>
+			</tr><tr>
+				<td>Glomesh Spice</td>
+				<td>1997</td>    
+			</tr><tr> 
+				<td>Madam President</td>
+				<td>1998</td>
+			<tr></tr>	
+				<td>OO-GLO</td>
+				<td>1999</td>
+			<tr></tr>	
+				<td>Glomesh Queen</td>
+				<td>2000</td>
+			<tr></tr>	
+				<td>Cruise control</td> 
+				<td>2001</td>
+			<tr></tr>	
+				<td>Glamourism</td>
+				<td>2002 </td>
+			<tr></tr>	
+				<td>Madge Kiss</td>
+				<td>2003</td>
+			<tr></tr>	
+				<td>Rehab</td> 
+				<td>2004</td>
+			<tr></tr>	
+				<td>Sedition</td>
+				<td>2005</td>
+			<tr></tr>	
+				<td>Adoption</td>
+				<td>2006</td>
+			<tr></tr>	
+				<td>Bag snatch</td>
+				<td>2007</td>
+			<tr></tr>	
+				<td>Lady Pope</td>
+				<td>2008</td>
+			<tr></tr>	
+				<td>Portable glamour</td> 
+				<td>2009</td>
+			<tr></tr>	
+				<td>Guerilla mesh</td> 
+				<td>2010</td>
+			<tr></tr>	
+				<td>Bowled over</td>
+				<td>2011</td>
+			<tr></tr>	
+				<td>Psy-ched up</td> 
+				<td>2012</td>
+			<tr></tr>	
+				<td>XRAY-ted</td> 
+				<td>2013</td>
+			</tr>
+		</table>
+	</div>
+</div>	
+<!-- footer -->
+<?php include_once('footer.php'); ?>
+</div> <!-- /container -->
 
-		<ul>
-
-			<li>Bonkers in Honkers	1995 </li>
-			<li>Glomesh Spice 		1997 </li>     
-			<li>Madam President		1998 </li>
-			<li>OO-GLO      		1999 </li>
-			<li>Glomesh Queen		2000 </li>
-			<li>Cruise control   	2001 </li>
-			<li>Glamourism  		2002 </li>
-			<li>Madge Kiss  		2003 </li>
-			<li>Rehab        		2004 </li>
-			<li>Sedition   			2005 </li>
-			<li>Adoption    		2006 </li>
-			<li>Bag snatch       	2007 </li>
-			<li>Lady Pope       	2008 </li>
-			<li>Portable glamour  	2009 </li>
-			<li>Guerilla mesh       2010 </li>
-			<li>Bowled over         2011 </li>
-			<li>Psy-ched up         2012 </li>
-			<li>XRAY-ted            2013 </li>
-
-		</ul>
-	
-
-	</aside>
-
-
-</section>
-
-<footer>
-DADAist amp; Glomesher Extraordinaire: 
-<a href="mailto:madam@madamlashby.com">madam@madamlashby.com</a>
-</footer>
 
 </body>
 </html>
